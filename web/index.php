ae448d2448005d003247c79a1bff37f2b881139c
unknown <dany_enterrador@hotmail.com>
2015-10-16 11:06:18 +0200

    Añadida extructura básica al fichero index.php.

---
+++ web/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <title>examen_ut1</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+    
+<body>
+
+    
+</body>
+</html>
